bc1e88c65ff0ec6ff43a08fad7aa44c126724139
Alisson Matos <alissonmgsantos@gmail.com>
2022-04-03 09:58:07 -0300

    feat: Adiciona tratamento para url inválida.

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,5 +1,24 @@
-document.querySelector('#form').addEventListener('submit', (event) => {
+const querySelector = (element) => document.querySelector(element);
+
+querySelector('#form').addEventListener('submit', (event) => {
   event.preventDefault();
-  const url = document.querySelector('#input_url').value;
-  window.location.replace(url);
+  let url = querySelector('#input_url').value;
+
+  const expression =
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+  if (expression.test(url)) {
+    if (!/(http(s?)):\/\//i.test(url)) {
+      url = 'http://' + url;
+    }
+    window.location.replace(url);
+  }
+  querySelector('#message_error').innerHTML = 'Your link is invalid.';
+});
+
+querySelector('#input_url').addEventListener('keyup', (event) => {
+  console.log(!!querySelector('#input_url').value);
+  if (!!querySelector('#input_url').value) {
+    querySelector('#message_error').innerHTML = null;
+  }
 });
